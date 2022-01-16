42bd419b8e856a08b72c15dd633a370654011342
hiverkiya <hiverkiya@gmail.com>
2022-01-16 19:17:15 +0530

    Added base build

---
+++ metaverse-media/components/Login.js
@@ -0,0 +1,31 @@
+import Image from "next/image"
+import {useMoralis} from 'react-moralis'
+function Login() {
+    const {authenticate}= useMoralis();
+    return (
+        <div>
+            <div className="flex flex-col absolute z-50 h-4/6 items-center justify-center w-full space-y-4">
+            <Image
+            className="object-cover rounded-full"
+            src="https://links.papareact.com/3pi"
+            height={200}
+            width={200}
+            />
+            <button 
+            onClick={ authenticate}
+            className="bg-yellow-500 rounded-lg p-5 font-bold  animate-pulse">Metaverse Media Login</button>
+            </div>
+
+            <div className="w-full h-screen ">
+            <Image
+            src="https://links.papareact.com/55n"
+            layout="fill"
+            objectFit="cover"
+            
+            />
+            </div>
+        </div>
+    )
+}
+
+export default Login
