8397c934734abda765aba2832f6192fe28c4f71d
hiverkiya <hiverkiya@gmail.com>
2022-01-19 17:40:33 +0530

    fixing bugs

--- metaverse-media/components/Login.js
+++ metaverse-media/components/Login.js
@@ -7,18 +7,18 @@ function Login() {
             <div className="flex flex-col absolute z-50 h-4/6 items-center justify-center w-full space-y-4">
             <Image
             className="object-cover rounded-full"
-            src="https://links.papareact.com/3pi"
+            src="https://raw.githubusercontent.com/hiverkiya/Metaverse-Media/main/metaverse-media/media/ethereum.png"
             height={200}
             width={200}
             />
             <button 
             onClick={ authenticate}
-            className="bg-yellow-300 rounded-lg p-5 font-bold  animate-pulse">Metaverse Media Login</button>
+            className="bg-red-600 rounded-lg p-5 font-bold text-lg  animate-pulse">LOGIN TO METAVERSE MEDIA</button>
             </div>
 
             <div className="w-full h-screen ">
             <Image
-            src="https://raw.githubusercontent.com/hiverkiya/Metaverse-Media/d788b57e7847aa4d9dcc931029e5d678b50c0d87/metaverse-media/media/bg.jpg"
+            src="https://raw.githubusercontent.com/hiverkiya/Metaverse-Media/main/metaverse-media/media/bg.jpg"
             layout="fill"
             objectFit="cover"
             
